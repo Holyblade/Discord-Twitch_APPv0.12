350be5c831d768c219ac32f8e591b27894f7496f
Jorgi Bolonhezi <luiz.bolonhezi.dias@gmail.com>
2022-06-29 00:25:07 -0400

    Update Filter.java
    Signed-off-by: Jorgi Bolonhezi <luiz.bolonhezi.dias@gmail.com>

--- src/ReadAndWriteChat/Filter.java
+++ src/ReadAndWriteChat/Filter.java
@@ -1,6 +1,5 @@
 package ReadAndWriteChat;
 
-import javax.swing.JOptionPane;
 import OpenFile.File;
 
 public class Filter {
@@ -8,14 +7,12 @@ public class Filter {
 	public static void main(String[] Args) throws Exception {
 
 		try {
-			String Input_Directory = JOptionPane.showInputDialog(null, "Entry Path: ", "Discord-Twitch_APPv0.1 - Fill in the Required Information", JOptionPane.QUESTION_MESSAGE);
-			String Output_Directory = JOptionPane.showInputDialog(null, "Output Directory with the / in the end: ", "Discord-Twitch_APPv0.1 - Fill in the Required Information", JOptionPane.QUESTION_MESSAGE);
 			File Tokens = new File();
-			Tokens.ReadFile(Input_Directory, Output_Directory);
+			Tokens.ReadFile(Args[0]);
 			int processedTokens = Tokens.getprocessedTokens(), removedTokens = Tokens.getremovedTokens();
-			JOptionPane.showMessageDialog(null, "Chat Tokens Successfully Generated!" + "\n Tokens Processed: " + processedTokens + "\n Removed Tokens: " + removedTokens);
+			System.out.println("Chat Tokens Successfully Generated!" + "\nTokens Processed: " + processedTokens + "\nRemoved Tokens: " + removedTokens);
 		} catch (Exception Error) {
-			JOptionPane.showMessageDialog(null, "Error:"+Error);
+			System.out.println("Error:"+Error);
 		}
 		
 	}
