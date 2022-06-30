c91eaee4dfa557095788ba30a3edee74e8fc993e
Jorgi Bolonhezi <luiz.bolonhezi.dias@gmail.com>
2022-06-30 14:50:58 -0400

    Update Filter.java
    Signed-off-by: Jorgi Bolonhezi <luiz.bolonhezi.dias@gmail.com>

--- src/ReadAndWriteChat/Filter.java
+++ src/ReadAndWriteChat/Filter.java
@@ -9,8 +9,8 @@ public class Filter {
 		try {
 			File Tokens = new File();
 			Tokens.ReadFile(Args[0]);
-			int processedTokens = Tokens.getprocessedTokens(), removedTokens = Tokens.getremovedTokens();
-			System.out.println("Chat Tokens Successfully Generated!" + "\nTokens Processed: " + processedTokens + "\nRemoved Tokens: " + removedTokens);
+			int processedTokens = Tokens.getprocessedTokens(), removedTokens = Tokens.getremovedTokens(), readyTokens = Tokens.getreadyTokens();
+			System.out.println("Chat Tokens Successfully Generated!" + "\nTokens Processed: " + processedTokens + "\nTokens Removed: " + removedTokens + "\nReady Tokens: "+readyTokens);
 		} catch (Exception Error) {
 			System.out.println("Error:"+Error);
 		}
